9af8fcf30ee4095d2bf59ffbdc83676676fd8225
nasdj21 <nasierra@espol.edu.ec>
2024-12-09 14:40:25 -0500

    Se agregaron los credentials

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+import { firebaseConfig } from './credentials'; //Importo las credenciales
+
+ /* Importe los módulos de AngularFire */
+ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+
+    /* Inyecte los módulos de AngularFire */
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
 });
