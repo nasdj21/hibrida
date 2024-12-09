515c963ceb4b7a3f8ea040b5a186418857cc5dba
nasdj21 <nasierra@espol.edu.ec>
2024-12-09 13:59:52 -0500

    Se agrego el form en el tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonSelect, IonSelectOption, IonTextarea,IonButton, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
@@ -7,7 +7,10 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent,
+    IonSelect, IonSelectOption, IonTextarea,IonButton,
+    IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, 
+    ExploreContainerComponent]
 })
 export class Tab2Page {
 
